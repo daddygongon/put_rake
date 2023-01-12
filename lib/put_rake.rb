035d85feaa3527c00db5314727e620b7134c75f4
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2023-01-12 18:57:57 +0900

    first version

--- lib/put_rake.rb
+++ lib/put_rake.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
+require "thor"
+require "fileutils"
+require "pp"
+require "yaml"
+require "command_line/global"
 
 require_relative "put_rake/version"
-
+#require_relative "put_rake/cli"
 module PutRake
   class Error < StandardError; end
   # Your code goes here...
+  class CLI < Thor
+    package_name 'my_help'
+    map "-v" => :version
+    map "--version" => :version
+
+    desc "version", "show version"
+
+    def version
+      print "put_rake #{VERSION}"
+    end
+
+    desc "for [EXT]", "for Rakefile about [EXT]"
+    def for(*args)
+      gem_template_dir = File.join(File.dirname(__FILE__), 'templates')
+      file = "Rakefile_#{args[0]}"
+      if File.exists?("./Rakefile")
+        puts "Rakefile exists."
+      else
+        comm = "cp #{File.join(gem_template_dir,file)} ./Rakefile"
+        system comm
+      end
+    end
+
+    desc "list", "list available Rakefiles"
+    def list
+      gem_template_dir = File.join(File.dirname(__FILE__), 'templates')
+      files = File.join(gem_template_dir,'*')
+      Dir.glob(files).each do |file|
+        puts File.basename(file)
+      end
+    end
+
+  end
 end
--- put_rake.gemspec
+++ put_rake.gemspec
@@ -13,7 +13,7 @@ Gem::Specification.new do |spec|
   spec.license = "MIT"
   spec.required_ruby_version = ">= 2.6.0"
 
-  spec.metadata["source_code_uri"] = "https://github.com/daddygonogon"
+  spec.metadata["source_code_uri"] = "https://github.com/daddygonogon/put_rake"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -27,8 +27,11 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   # Uncomment to register a new dependency of your gem
-  # spec.add_dependency "example-gem", "~> 1.0"
-
+  spec.add_development_dependency "rspec"
+  spec.add_runtime_dependency "thor"
+  spec.add_runtime_dependency "command_line"
+  spec.add_runtime_dependency "colorize"
+  #spec.add_development_dependency "aruba"
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
 end
