0ab0296c3a63c897622626d7373546e1bd5dcdb6
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2023-01-12 18:17:43 +0900

    first commit

---
+++ lib/put_rake.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "put_rake/version"
+
+module PutRake
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/put_rake_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PutRake do
+  it "has a version number" do
+    expect(PutRake::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
